9822337a3b6377f9455fa25b39cfee8a2d278d7a
BeatrizLandi <beatriz.landi.coelho@gmail.com>
2024-05-10 10:11:03 -0300

    tables

---
+++ src/main/java/com/example/demo/models/Category.java
@@ -0,0 +1,46 @@
+package com.example.demo.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+@Entity
+public class Category {
+
+    //atributos
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; 
+
+    @NotEmpty
+    @Size(min=2,max = 255,message = "name must be betwenn 2 and 255")
+    private String name;
+
+    //acessores
+    public Category(){
+
+    }
+
+    public Category(String name){
+        this.name = name;
+
+    }
+
+    //gettres settes
+
+    public Long getId(){
+        return this.id;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    //metodos
+}
---
+++ src/main/java/com/example/demo/models/Product.java
@@ -0,0 +1,84 @@
+package com.example.demo.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+public class Product {
+    
+    //atributos
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private Long id;
+
+   @NotBlank
+   @Size(min=2,max=255,message="name must be betwenn 2 and 255")
+   private String name;
+
+   @NotBlank
+   @Size(min=2,max=255,message="name must be betwenn 2 and 255")
+   private String description;
+
+   @NotBlank
+   @Min(value = 0, message = "Price must be greater than or equal to 0")
+   private double price; 
+
+   @NotBlank
+   @Min(value = 0, message = "Stocl must be greater than or equal to 0")
+   private int stock;
+
+    //acesores
+    public Product(){
+
+    }
+
+    public Product(String name,String description,double price,int stock){
+
+        this.name=name;
+        this.description=description;
+        this.price=price;
+        this.stock=stock;
+
+    }
+
+    //getters setters
+    public Long getId(){
+        return this.id;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getDescription(){
+        return this.description;
+    }
+    public void setDescription(String description){
+        this.description = description;
+    }
+
+    public double getPrice(){
+        return this.price;
+    }
+    public void setPrice(double price){
+        this.price = price;
+    }
+
+    public int geStock(){
+        return this.stock;
+    }
+    public void setStock(int stock){
+        this.stock = stock;
+    }
+
+    //metodos
+    
+}
